aff4b229b8c1e95447e3153194c8ce121a4c9e4d
Ivan Krišto <ivan.kristo@gmail.com>
2016-07-24 15:20:54 +0200

    Fix tesseract integration

--- src/Comic/OCR.hs
+++ src/Comic/OCR.hs
@@ -1,19 +1,35 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Comic.OCR where
+module Comic.OCR
+    ( ocrImage
+    ) where
 
-import Data.ByteString hiding (pack)
-import Data.Text
---import Text.OCR.Tesseract
+import           HSH.Command ((-|-), ShellCommand(..), run)
+import           HSH.ShellEquivs (echo)
+import qualified Data.ByteString as B
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
--- tesseract stdin stdout -l eng --tessdata-dir=$PWD/tessdata -psm 1
+tesseractCmd :: String
+tesseractCmd = "tesseract"
+
+buildTesseractArgs :: String -> String -> [String]
+buildTesseractArgs tessdataPath lang =
+  [ "stdin"
+  , "stdout"
+  , "-l " ++ lang
+  ,"--tessdata-dir='" ++ tessdataPath ++ "'"
+  , "-psm 1"
+  ]
 
 -- | Read written text from an image.
-ocrImage :: ByteString -> Text -> Text -> IO Text
-ocrImage imgBS tesseractCfgPath lang = return "dummy"
---    do let config = TesseractConfig (Just tesseractCfgPath) lang
---       res <- withTesseractConfig config $ withImageBS imgBS getRecognizedText
---       return $ cleanText res
+ocrImage :: B.ByteString -> T.Text -> T.Text -> IO T.Text
+ocrImage imageContent tessdataPath lang =
+  do res <- run $ echo imageContent -|- (tesseractCmd, tesseractArgs)
+     return $ cleanText $ T.decodeUtf8 res
+  where
+    tesseractArgs :: [String]
+    tesseractArgs = buildTesseractArgs (T.unpack tessdataPath) (T.unpack lang)
 
-cleanText :: Text -> Text
-cleanText = replace (pack "\n") (pack " ") 
+    cleanText :: T.Text -> T.Text
+    cleanText = T.replace (T.pack "\n") (T.pack " ") 
